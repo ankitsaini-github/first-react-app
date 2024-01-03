203135e81739e177dbfe0a01c131dc4ddbd60a5a
ankitsaini-github <ankit121saini@gmail.com>
2024-01-04 04:46:52 +0530

    added expense location

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,6 +6,7 @@ function ExpenseItem(props) {
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
+        <div className="expense-item__location">{props.location}</div>
         <h2>{props.title}</h2>
         <div className="expense-item__price">Rs. {props.amount}</div>
       </div>
