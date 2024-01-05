de9e906b303d5fe9cda652027d39b51c58998b4a
ankitsaini-github <ankit121saini@gmail.com>
2024-01-05 14:10:21 +0530

    update amount (useState)

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,12 +1,17 @@
+import React, {useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import ExpenseDetails from "./ExpenseDetails";
 
 const ExpenseItem = (props) => {
+  const [amount,setAmount] = useState(props.amount);
+
+  const updateAmount =()=>{
+    setAmount(100);
+  }
 
   const deleteExpense =()=>{
-
     if(window.confirm(`Delete expense : ${props.title} ?`)){
       let d= document.getElementById(props.id)
       d.remove();
@@ -15,8 +20,12 @@ const ExpenseItem = (props) => {
   return (
     <Card className="expense-item" id={props.id}>
         <ExpenseDate date={props.date}/>
-        <ExpenseDetails location={props.location} title={props.title} amount={props.amount}/>
+        <ExpenseDetails location={props.location} title={props.title} amount={amount}/>
+        <div className="btn-group">
+        <button className="add-amount" onClick={updateAmount}>Update Amount</button>
         <button className="del-btn" onClick={deleteExpense}>Delete Expense</button>
+        </div>
+
     </Card>
   );
 }
