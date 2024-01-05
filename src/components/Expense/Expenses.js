ad6e94ccc9f65c0135b3ec64c9ea452ad2a05221
ankitsaini-github <ankit121saini@gmail.com>
2024-01-06 03:53:01 +0530

    expense filter placeholder added

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem.js";
 import "./Expenses.css";
 import Card from "../UI/Card.js";
+import ExpenseFilter from "./ExpenseFilter.js";
+
 const Expenses = (props) => {
+  const [filterYear,setFilterYear]=useState('2020');
+  const onFilterChangeHandler =(selectedYear)=>{
+    setFilterYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+        <ExpenseFilter selected={filterYear} onChangeFilter={onFilterChangeHandler}/>
         {props.items.map((item) => (
             <ExpenseItem
             key={item.id}
