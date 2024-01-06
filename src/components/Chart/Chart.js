b8b3e9dcf491c0984ede62cbac7421ab246c92fc
ankitsaini-github <ankit121saini@gmail.com>
2024-01-06 22:59:05 +0530

    added chart component

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,21 @@
+import "./Chart.css";
+import Chartbar from "./Charbar";
+const Chart = (props) => {
+  const dataPointValueArray = props.dataPoints.map((dataPoint) => dataPoint.value );
+  const MaximumValue = Math.max(...dataPointValueArray);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <Chartbar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={MaximumValue}
+          label={dataPoint.label}
+        />)
+      )}
+    </div>
+  );
+};
+
+export default Chart;
--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import Card from "../UI/Card.js";
 import ExpenseFilter from "./ExpenseFilter.js";
 import ExpenseList from "./ExpenseList.js"
+import ExpenseChart from "./ExpenseChart.js";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState("");
@@ -19,6 +20,7 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseFilter onChangeFilter={onFilterChangeHandler} />
+      <ExpenseChart expenses={filteredExpense}/>
       <ExpenseList items={filteredExpense} year={filterYear}/>
     </Card>
   );
